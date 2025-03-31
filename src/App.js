2ce242dbb5dbf7e42df2eacc4ca9e960632407ad
Hemanthsure123 <hemanthsure3@gmail.com>
2025-04-01 01:08:06 +0530

    allnew

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
 import './App.css';
+import { jwtDecode } from 'jwt-decode'
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [user, setUSer] = useState({});
+
+    function handleCallbackResponse(response) {
+        console.log("Encoded JWT ID token: " + response.credential);
+        const userObject = jwtDecode(response.credential);
+        console.log(userObject)
+        setUSer(userObject)
+        document.getElementById("signInDiv").hidden = true
+    }
+
+    function handleSignOut(event) {
+        event.preventDefault()
+        setUSer({})
+        document.getElementById("signInDiv").hidden = false
+    }
+
+    useEffect(() => {
+        /* global google */
+        google.accounts.id.initialize({
+            client_id: "101102035330-lsh20nibjlp3drt6e9fg5oa4ajkgo9qg.apps.googleusercontent.com",
+            callback: handleCallbackResponse
+        });
+
+        google.accounts.id.renderButton(
+            document.getElementById("signInDiv"), { theme: "outline", size: "large" }
+        );
+
+        google.accounts.id.prompt()
+
+    }, []);
+
+    return ( <
+        div className = 'App' >
+        <
+        div id = "signInDiv" > < /div> {
+            Object.keys(user).length !== 0 &&
+                <
+                div className = 'account-card' >
+                <
+                h1 className = 'account-heading' > My Account < /h1> <
+                div className = 'account-details' >
+                <
+                img src = { user.picture }
+            alt = "logo"
+            className = 'account-image' / >
+                <
+                h3 className = 'account-name' > { user.name } < /h3> <
+                /div> <
+                p className = 'account-email' > { user.email } < /p> <
+                button onClick = {
+                    (e) => handleSignOut(e) }
+            className = 'sign-out-button' > Sign Out < /button> <
+                /div>  
+        } <
+        /div>
+
+    );
 }
 
 export default App;
